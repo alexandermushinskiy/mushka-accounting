b23b5d1f8664546916f042edaf21cf6e0a3e94c9
alexandermushinskiy <alexander.mushinskiy@sigma.software>
2019-01-04 16:31:16 +0200

    Add ukrainian regions

--- frontend/src/app/orders/order/order.component.ts
+++ frontend/src/app/orders/order/order.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { NotificationsService } from '../../core/notifications/notifications.service';
 import { OrdersService } from '../../core/api/orders.service';
 import { Order } from '../../shared/models/order.model';
+import { OrderProduct } from '../../shared/models/order-product.model';
+import { Product } from '../../shared/models/product.model';
+import { ProductsServce } from '../../core/api/products.service';
+import { ukrRegions } from '../shared/constants/urk-regions.const';
 
 @Component({
   selector: 'mk-order',
@@ -19,14 +23,25 @@ export class OrderComponent implements OnInit {
   orderId: string;
   errors: string[];
   title: string;
+  productsList: Product[];
+  regions = ukrRegions;
 
   constructor(private formBuilder: FormBuilder,
               private route: ActivatedRoute,
               private router: Router,
               private ordersService: OrdersService,
+              private productsService: ProductsServce,
               private notificationsService: NotificationsService) { }
 
   ngOnInit() {
+    this.productsService.getAll()
+      .subscribe((products: Product[]) => {
+        this.productsList = products;
+        this.getRouteParams();
+      });
+  }
+
+  private getRouteParams() {
     this.route.params.subscribe(params => {
       this.orderId = params['id'];
       this.isEdit = !!this.orderId;
@@ -36,9 +51,30 @@ export class OrderComponent implements OnInit {
         this.ordersService.getById(this.orderId)
           .subscribe((order: Order) => this.buildForm(order));
       } else {
-        this.buildForm(new Order({}));
+        this.buildForm(new Order({
+          cost: 0,
+          products: [new OrderProduct({})]
+        }));
       }
     });
+  }
+
+  addProduct() {
+    const products = <FormArray>this.orderForm.get('products');
+    products.push(this.createProductModel(new OrderProduct({})));
+  }
+
+  removeProduct(index: number) {
+    const products = <FormArray>this.orderForm.get('products');
+    products.removeAt(index);
+  }
+
+  getProductSize(product: Product): string {
+    if (!product) {
+      return '';
+    }
+
+    return !!product.size ? product.size.name : '-';
   }
 
   saveOrder() {
@@ -46,7 +82,20 @@ export class OrderComponent implements OnInit {
 
   private buildForm(order: Order) {
     this.orderForm = this.formBuilder.group({
+      cost: [{value: order.cost, disabled: true}],
+      costMethod: [order.costMethod, Validators.required],
+      notes: [order.notes],
+      products: this.formBuilder.array(
+        order.products.map(param => this.createProductModel(param))
+      )
+    });
+  }
 
+  private createProductModel(productItem: OrderProduct): FormGroup {
+    return this.formBuilder.group({
+      product: [productItem.product],
+      quantity: [productItem.quantity, Validators.required],
+      costForItem: [productItem.costForItem, Validators.required]
     });
   }
 }
--- frontend/src/app/shared/models/order.model.ts
+++ frontend/src/app/shared/models/order.model.ts
@@ -1,6 +1,13 @@
+import { OrderProduct } from './order-product.model';
+import { PaymentMethod } from '../enums/payment-method.enum';
+
 export class Order {
   id: string;
   orderDate: string;
+  cost: number;
+  costMethod: PaymentMethod;
+  notes: string;
+  products: OrderProduct[];
 
   constructor(data: any) {
     Object.assign(this, data);
