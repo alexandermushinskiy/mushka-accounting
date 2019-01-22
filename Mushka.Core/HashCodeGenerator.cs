eb576e74ef978503bcc4f731c9071662289322fd
alexandermushinskiy <amushinskiy@gmail.com>
2019-01-22 21:28:21 +0200

    Added hashcode generator. Initial script is updated.

---
+++ Mushka.Core/HashCodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Mushka.Core
+{
+    public static class HashCodeGenerator
+    {
+        public static int GetFromValues(params object[] values) => GetFromValues(values.ToList());
+
+        public static int GetFromValues(IEnumerable<object> values) => GetFromHashCodes(values.Where(value => value != null).Select(value => value.GetHashCode()).ToArray());
+
+        public static int GetFromHashCodes(params int[] hashCodes) => GetFromHashCodes(hashCodes.ToList());
+
+        public static int GetFromHashCodes(IEnumerable<int> hashCodes) => hashCodes.Aggregate(17, (current, value) => (current * 59) + value);
+    }
+}
